645773f0e674ab00523f638cdc77fa1746d38e51
John Christian D. Rosuelo <152044019+AetherKnowledge@users.noreply.github.com>
2025-06-30 01:44:39 +0800

    Google login, Hero Page, Text shadow

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,10 +6,12 @@ import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <div className="navbar bg-base-100 p-5">
+    <div className="navbar bg-base-100/80 p-5 fixed top-0 left-0 w-full z-1">
       <div className="navbar-start">
         <Image src={lcupLogo} alt="logo" width={60} height={60} priority />
-        <h1 className="text-base-content font-bold pl-3">LCUP Mental Care</h1>
+        <h1 className="text-base-content font-bold pl-3 text-shadow-br">
+          LCUP Mental Care
+        </h1>
       </div>
       <div className="navbar-center pl-10">
         <div className="flex items-center space-x-15">

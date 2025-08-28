81208dfd6e97bbfacb6eb59da59d3c5b07499699
John Christian D. Rosuelo <152044019+AetherKnowledge@users.noreply.github.com>
2025-08-28 22:04:05 +0800

    chore: update dependencies and improve Prisma schema
    - Updated various dependencies in package.json for better performance and security.
    - Modified Prisma schema to introduce a new User model and refactor existing models to use UUIDs as primary keys.
    - Added new relationships and constraints in the schema for better data integrity.
    - Enhanced seed script to create an admin user and set up necessary grants and policies for the new schema.
    - Created migration script to handle schema changes and ensure smooth transition to the new structure.
    - Now using supabase with next auth
    - Socket wont connect if user is not authenticated

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,18 +1,25 @@
 // lib/supabaseClient.ts
 import { createBrowserClient } from "@supabase/ssr";
 
-export function createClient() {
+export function createClient(supabaseAccessToken: string) {
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      global: {
+        headers: {
+          Authorization: `Bearer ${supabaseAccessToken}`,
+        },
+      },
+    }
   );
 }
 
-export async function isUserLoggedIn() {
-  const supabase = createClient();
-  const { data: session } = await supabase.auth.getSession();
-  return !(!session.session || session.session.user.role !== "authenticated");
-}
+// export async function isUserLoggedIn() {
+//   const supabase = createClient();
+//   const { data: session } = await supabase.auth.getSession();
+//   return !(!session.session || session.session.user.role !== "authenticated");
+// }
 
 // export const signInWithGoogle = async () => {
 //   const { data, error } = await supabase.auth.signInWithOAuth({
@@ -22,4 +29,11 @@ export async function isUserLoggedIn() {
 //   return data;
 // };
 
-// export const bucket = supabase.storage.from("capstone");
+export enum Buckets {
+  Capstone = "capstone",
+  Posts = "posts",
+}
+
+export function getBucket(bucketName: Buckets, supabaseAccessToken: string) {
+  return createClient(supabaseAccessToken).storage.from(bucketName);
+}

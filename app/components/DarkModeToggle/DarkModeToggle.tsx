d3c9cf7f3738919e171b225eb5f1cf0d64d24691
John Christian D. Rosuelo <152044019+AetherKnowledge@users.noreply.github.com>
2025-10-06 06:15:30 +0800

    fix: Ensure correct theme is applied on dark mode toggle error

--- app/components/DarkModeToggle/DarkModeToggle.tsx
+++ app/components/DarkModeToggle/DarkModeToggle.tsx
@@ -5,8 +5,12 @@ import { changeDarkMode } from "./DarkModeActions";
 const DarkModeToggle = () => {
   const handleToggle = async (enabled: boolean) => {
     try {
+      const newTheme = enabled ? "lcup-dark" : "lcup-light";
+      document.documentElement.setAttribute("data-theme", newTheme);
       await changeDarkMode(enabled);
     } catch (error) {
+      const oldTheme = enabled ? "lcup-light" : "lcup-dark";
+      document.documentElement.setAttribute("data-theme", oldTheme);
       console.error("Error changing dark mode:", error);
     }
   };
